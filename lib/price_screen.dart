2d5ef5e5922793bc7d45ee13b3438d40586fe067
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-04-15 20:38:11 +0530

    initial commit!!

---
+++ lib/price_screen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'coin_data.dart';
+
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = "USD";
+
+  DropdownButton androidDropDown() {
+    List<DropdownMenuItem<String>> currency = [];
+    for (String item in currenciesList) {
+      var curr = DropdownMenuItem(
+        child: Text(item),
+        value: item,
+      );
+      currency.add(curr);
+    }
+
+    return DropdownButton<String>(
+        value: selectedCurrency,
+        items: currency,
+        onChanged: (value) {
+          setState(() {
+            selectedCurrency = value;
+          });
+        });
+  }
+
+  CupertinoPicker iosPicker() {
+    List<Text> currency = [];
+    for (String item in currenciesList) {
+      currency.add(Text(
+        item,
+        style: TextStyle(color: Colors.white),
+      ));
+    }
+    return CupertinoPicker(
+      itemExtent: 32.0,
+      onSelectedItemChanged: (item) {
+        print(item);
+      },
+      offAxisFraction: 0.5,
+      children: currency,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("🤑 Coin Ticker"),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+              child: Card(
+                color: Colors.grey[850],
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                  child: Text(
+                    '1 BTC = ? USD',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      stops: [
+                    0.5,
+                    0.5,
+                  ],
+                      colors: [
+                    Colors.black,
+                    Colors.purple
+                  ])),
+              child: androidDropDown() ,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
