980bec360dfd38e5f7e91af8f13fedfb83a88cb8
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-04-16 00:50:45 +0530

    completed code

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,21 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  final String url = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+  String symbol;
+  String currency;
+  CoinData({this.symbol, this.currency});
+  Future<int> getData() async {
+    http.Response response = await http.get("$url$symbol$currency",
+        headers: {"x-ba-key": "MGRmZTNlNWM5YjZlNDY0MmE3ZjdhMGI1NmQ3MmRiNGM"});
+
+    if (response.statusCode == 200) {
+      Map data = jsonDecode(response.body);
+      int last = data["last"].floor();
+      return last;
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io' show Platform;
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +11,10 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  String selectedCurrency = "USD";
+  String selectedCurrency = "Currency";
+  int currencyResultBTC = 0;
+  int currencyResultETH = 0;
+  int currencyResultLTC = 0;
 
   DropdownButton androidDropDown() {
     List<DropdownMenuItem<String>> currency = [];
@@ -24,9 +29,21 @@ class _PriceScreenState extends State<PriceScreen> {
     return DropdownButton<String>(
         value: selectedCurrency,
         items: currency,
-        onChanged: (value) {
+        onChanged: (value) async {
+          selectedCurrency = value;
+          currencyResultBTC =
+              await CoinData(symbol: "BTC", currency: selectedCurrency)
+                  .getData();
+          currencyResultETH =
+              await CoinData(symbol: "ETH", currency: selectedCurrency)
+                  .getData();
+          currencyResultLTC =
+              await CoinData(symbol: "LTC", currency: selectedCurrency)
+                  .getData();
           setState(() {
-            selectedCurrency = value;
+            currencyResultBTC = currencyResultBTC;
+            currencyResultETH = currencyResultETH;
+            currencyResultLTC = currencyResultLTC;
           });
         });
   }
@@ -40,13 +57,33 @@ class _PriceScreenState extends State<PriceScreen> {
       ));
     }
     return CupertinoPicker(
+      looping: false,
       itemExtent: 32.0,
-      onSelectedItemChanged: (item) {
-        print(item);
+      onSelectedItemChanged: (item) async {
+        selectedCurrency = currenciesList[item];
+        currencyResultBTC =
+            await CoinData(symbol: "BTC", currency: selectedCurrency).getData();
+        currencyResultETH =
+            await CoinData(symbol: "ETH", currency: selectedCurrency).getData();
+        currencyResultLTC =
+            await CoinData(symbol: "LTC", currency: selectedCurrency).getData();
+        setState(() {
+          currencyResultBTC = currencyResultBTC;
+          currencyResultETH = currencyResultETH;
+          currencyResultLTC = currencyResultLTC;
+        });
+        print(
+            "currencyResultBTC = $currencyResultBTC, currencyResultETH = $currencyResultETH, currencyResultLTC = $currencyResultLTC");
       },
-      offAxisFraction: 0.5,
+      offAxisFraction: 0.8,
       children: currency,
     );
+  }
+
+  Widget getFeature() {
+    if (Platform.isAndroid)
+      return iosPicker(); //use this androidDropDown(); for ugly picker
+    if (Platform.isIOS) return iosPicker();
   }
 
   @override
@@ -64,47 +101,77 @@ class _PriceScreenState extends State<PriceScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Padding(
-              padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-              child: Card(
-                color: Colors.grey[850],
-                elevation: 5.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Padding(
-                  padding:
-                      EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                  child: Text(
-                    '1 BTC = ? USD',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
+            CustomCards(
+              symbol: "BTC",
+              currencyResult: currencyResultBTC,
+              currency: selectedCurrency,
+            ),
+            CustomCards(
+              symbol: "ETH",
+              currencyResult: currencyResultETH,
+              currency: selectedCurrency,
+            ),
+            CustomCards(
+              symbol: "LTC",
+              currencyResult: currencyResultLTC,
+              currency: selectedCurrency,
             ),
             Container(
               height: 150.0,
               alignment: Alignment.center,
               padding: EdgeInsets.only(bottom: 30.0),
               decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
+                  gradient: RadialGradient(
+//                      begin: Alignment.topLeft,
+//                      end: Alignment.bottomRight,
                       stops: [
-                    0.5,
-                    0.5,
-                  ],
-                      colors: [
-                    Colors.black,
-                    Colors.purple
-                  ])),
-              child: androidDropDown() ,
+                    0.7,
+                    0.9,
+                    0.1
+                  ], colors: [
+                Colors.black,
+                Colors.white,
+                Colors.transparent
+              ])),
+              child: getFeature(),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomCards extends StatefulWidget {
+  final int currencyResult;
+  final String symbol;
+  final String currency;
+  CustomCards({this.symbol, this.currencyResult, this.currency});
+  @override
+  _CustomCardsState createState() => _CustomCardsState();
+}
+
+class _CustomCardsState extends State<CustomCards> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.grey[850],
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 ${widget.symbol} = ${widget.currencyResult} ${widget.currency}',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
         ),
       ),
     );
